47f4989fea4f0178071ab09b850675e0cb5f5e0e
Steka <stephenkaruru05@gmail.com>
2025-06-21 18:52:56 +0300

    step1

---
+++ components/WalletProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import "@mysten/dapp-kit/dist/index.css";
+
+const queryClient = new QueryClient();
+
+const networks = {
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+};
+
+export default function WalletProviderWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork="testnet">
+        <WalletProvider>{children}</WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
+}
